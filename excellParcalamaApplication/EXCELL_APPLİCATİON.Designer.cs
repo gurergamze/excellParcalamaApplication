828f2d14c13148dd5edc3b3eaebc0983867db526
ggurer <ggurer@N110767>
2024-08-08 17:04:47 +0300

    Add project files.

---
+++ excellParcalamaApplication/EXCELL_APPLİCATİON.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace excellParcalamaApplication
+{
+    partial class EXCELL_APPLİCATİON
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_parcala = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_colomn_name = new System.Windows.Forms.TextBox();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.mailBody = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // button_parcala
+            // 
+            this.button_parcala.BackColor = System.Drawing.Color.LightGray;
+            this.button_parcala.ForeColor = System.Drawing.Color.DarkRed;
+            this.button_parcala.Location = new System.Drawing.Point(12, 65);
+            this.button_parcala.Name = "button_parcala";
+            this.button_parcala.Size = new System.Drawing.Size(275, 58);
+            this.button_parcala.TabIndex = 0;
+            this.button_parcala.Text = "EMAIL GONDER";
+            this.button_parcala.UseVisualStyleBackColor = false;
+            this.button_parcala.Click += new System.EventHandler(this.button_parcala_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SÜTUN İSMİNİ GİRİNİZ";
+            // 
+            // textBox_colomn_name
+            // 
+            this.textBox_colomn_name.Location = new System.Drawing.Point(142, 28);
+            this.textBox_colomn_name.Name = "textBox_colomn_name";
+            this.textBox_colomn_name.Size = new System.Drawing.Size(145, 20);
+            this.textBox_colomn_name.TabIndex = 2;
+            // 
+            // button_exit
+            // 
+            this.button_exit.BackColor = System.Drawing.Color.LightGray;
+            this.button_exit.ForeColor = System.Drawing.Color.DarkRed;
+            this.button_exit.Location = new System.Drawing.Point(12, 129);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(275, 58);
+            this.button_exit.TabIndex = 3;
+            this.button_exit.Text = "ÇIKIŞ";
+            this.button_exit.UseVisualStyleBackColor = false;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // mailBody
+            // 
+            this.mailBody.Location = new System.Drawing.Point(12, 243);
+            this.mailBody.Multiline = true;
+            this.mailBody.Name = "mailBody";
+            this.mailBody.Size = new System.Drawing.Size(275, 233);
+            this.mailBody.TabIndex = 4;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // EXCELL_APPLİCATİON
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(299, 486);
+            this.Controls.Add(this.mailBody);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.textBox_colomn_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_parcala);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "EXCELL_APPLİCATİON";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.EXCELL_APPLİCATİON_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_parcala;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_colomn_name;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.TextBox mailBody;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ excellParcalamaApplication/EXCELL_APPLİCATİON.cs
@@ -0,0 +1,371 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+namespace excellParcalamaApplication
+{
+    public partial class EXCELL_APPLİCATİON : Form
+    {
+
+        OpenFileDialog openFileDialog1 = new OpenFileDialog()
+        {
+            Filter = "Excel Dosyası |*.xlsx| Excel Dosyası|*.xls",
+            // open file dialog açıldığında sadece excel dosyalarınu görecek
+            Title = "Excel Dosyası Seçiniz..",
+            // open file dialog penceresinin başlığı
+
+        };
+        OpenFileDialog openFileDialog2 = new OpenFileDialog()
+        {
+            Filter = "Excel Dosyası |*.xlsx| Excel Dosyası|*.xls",
+            Title = "Bayi VDM Bilgileri Dosyasını Seçiniz..",
+        };
+
+
+        public EXCELL_APPLİCATİON()
+        {
+
+            InitializeComponent();
+        }
+
+        private void SetDataTable_To_Excel(DataTable dtTable, string PathFileName)
+        {
+            string folderPath = @"C:\Users\ggurer\Desktop\ExcelDeneme";
+            string filePath = Path.Combine(folderPath, PathFileName + ".xlsx");
+
+
+            using (var workbook = new XLWorkbook())
+            {
+                workbook.Worksheets.Add(dtTable, "Deneme");
+                workbook.SaveAs(filePath);
+            }
+        }
+
+
+
+        private void button_parcala_Click(object sender, EventArgs e)
+        {
+            //FileInfo fileInfo;
+            //string filepath2 = AppDomain.CurrentDomain.BaseDirectory.ToString() + @"ExcelExport\";
+            string folderPath = @"C:\Users\ggurer\Desktop\ExcelDeneme";
+
+
+            /* foreach (string dosya in Directory.GetFiles(filepath2))
+             {
+                 fileInfo = new FileInfo(dosya);
+                 fileInfo.Delete();
+
+             }*/
+            foreach (string dosya in Directory.GetFiles(folderPath))
+            {
+                FileInfo fileInfo = new FileInfo(dosya);
+                fileInfo.Delete();
+            }
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Parçalama İşlemi Başlatıldı. Lütfen Bekleyiniz..");
+                string DosyaYolu = openFileDialog1.FileName;// dosya yolu
+                string DosyaAdi = openFileDialog1.SafeFileName; // dosya adı
+
+                OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + DosyaYolu + "; Extended Properties='Excel 12.0 xml;HDR=YES;'");
+                OleDbCommand cmd = new OleDbCommand();
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT DISTINCT " + textBox_colomn_name.Text + " FROM [VDM_Kodu$]";
+                OleDbDataReader dr = cmd.ExecuteReader();
+
+
+                ArrayList VDM_kodlari = new ArrayList();
+
+                while (dr.Read())
+                {
+                    VDM_kodlari.Add(dr[textBox_colomn_name.Text]);
+                }
+                OleDbDataAdapter da = new OleDbDataAdapter("Select*from[VDM_Kodu$]", con);
+                DataTable DTexcel = new DataTable();
+                da.Fill(DTexcel);
+
+                dr.Close();
+                con.Close();
+
+
+
+                int sayac = 0;
+                sayac = VDM_kodlari.Count;
+                for (int i = 0; i < sayac; i++)
+                {
+
+                    if (VDM_kodlari[i].ToString().Length == 0)
+                        continue;
+
+                    var results = (from myRow in DTexcel.AsEnumerable()
+                                   where myRow.Field<string>(textBox_colomn_name.Text) == VDM_kodlari[i].ToString()
+                                   select myRow).CopyToDataTable();
+                    SetDataTable_To_Excel(results, VDM_kodlari[i].ToString());
+
+
+                }
+
+
+                MessageBox.Show("Parçalama İşlemi Tamamlandı.");
+
+
+               
+
+                if (openFileDialog2.ShowDialog() == DialogResult.OK)
+                {
+                    string vdmFilePath = openFileDialog2.FileName;
+
+                    if (!File.Exists(vdmFilePath))
+                    {
+                        MessageBox.Show("Dosya bulunamadı.");
+                        return;
+                    }
+
+                    DataTable dtVdmInfo = new DataTable();
+
+                    try
+                    {
+                        using (var workbook = new XLWorkbook(vdmFilePath))
+                        {
+                            var sheetNames = workbook.Worksheets.Select(ws => ws.Name).ToList();
+                            MessageBox.Show($"Mevcut sayfalar: {string.Join(", ", sheetNames)}");
+
+                            // Geçerli sayfa adıyla sayfayı seçme
+                            string sheetName = "Sayfa1"; 
+                            if (sheetNames.Contains(sheetName))
+                            {
+                                var worksheet = workbook.Worksheet(sheetName);
+                                var headerRow = worksheet.Row(1);
+                                if (headerRow.Cells().Count() == 0)
+                                {
+                                    MessageBox.Show("Başlık satırı bulunamadı.");
+                                    return;
+                                }
+
+                                foreach (var cell in headerRow.Cells())
+                                {
+                                    dtVdmInfo.Columns.Add(cell.GetValue<string>() ?? string.Empty);
+                                }
+
+                                for (int rowIndex = 2; rowIndex <= worksheet.LastRowUsed().RowNumber(); rowIndex++)
+                                {
+                                    var row = worksheet.Row(rowIndex);
+                                    if (row.Cells().All(cell => string.IsNullOrEmpty(cell.GetValue<string>())))
+                                    {
+                                        continue;
+                                    }
+
+                                    var dataRow = dtVdmInfo.NewRow();
+                                    for (int colIndex = 1; colIndex <= headerRow.Cells().Count(); colIndex++)
+                                    {
+                                        dataRow[colIndex - 1] = row.Cell(colIndex).GetValue<string>() ?? string.Empty;
+                                    }
+
+                                    dtVdmInfo.Rows.Add(dataRow);
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Sayfa '{sheetName}' bulunamadı. Lütfen geçerli bir sayfa adı girin.");
+                            }
+                        }
+
+                        MessageBox.Show("Dosya başarıyla okundu.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Dosya okuma hatası: {ex.Message}");
+                    }
+
+
+
+                    // Her VDM için genel toplam dosyası oluşturma
+                     foreach (string vdmKod in VDM_kodlari)
+                     {
+                         string vdmParcaFilePath = Path.Combine(folderPath, vdmKod + ".xlsx");
+                         if (File.Exists(vdmParcaFilePath))
+                         {
+                             DataTable vdmParcaTable = new DataTable();
+
+                             using (var workbook = new XLWorkbook(vdmParcaFilePath))
+                             {
+                                 var worksheet = workbook.Worksheet(1);
+                                 bool firstRow = true;
+
+                                 foreach (var row in worksheet.Rows())
+                                 {
+                                     if (firstRow)
+                                     {
+                                         foreach (var cell in row.Cells())
+                                             vdmParcaTable.Columns.Add(cell.Value.ToString());
+                                         firstRow = false;
+                                     }
+                                     else
+                                     {
+                                         vdmParcaTable.Rows.Add();
+                                         int i = 0;
+                                         foreach (var cell in row.Cells())
+                                             vdmParcaTable.Rows[vdmParcaTable.Rows.Count - 1][i++] = cell.Value;
+                                     }
+                                 }
+                             }
+
+                             // Genel toplam dosyası oluşturma
+                             var genelToplamResults = (from vdmRow in vdmParcaTable.AsEnumerable()
+                                                       join vdmBilgiRow in dtVdmInfo.AsEnumerable()
+                                                       on vdmRow.Field<string>(textBox_colomn_name.Text) equals vdmBilgiRow.Field<string>(textBox_colomn_name.Text)
+                                                       select new
+                                                       {
+                                                           vdmRow,
+                                                           vdmBilgiRow
+                                                       }).ToList();
+
+                             DataTable genelToplamTable = new DataTable();
+                             foreach (DataColumn column in vdmParcaTable.Columns)
+                             {
+                                 genelToplamTable.Columns.Add(column.ColumnName);
+                             }
+
+                             foreach (DataColumn column in dtVdmInfo.Columns)
+                             {
+                                 if (!genelToplamTable.Columns.Contains(column.ColumnName))
+                                     genelToplamTable.Columns.Add(column.ColumnName);
+                             }
+
+                             DataRow newRow = genelToplamTable.NewRow();
+                             foreach (DataColumn column in vdmParcaTable.Columns)
+                             {
+                                 newRow[column.ColumnName] = genelToplamResults[0].vdmRow[column];
+                             }
+
+                             foreach (DataColumn column in dtVdmInfo.Columns)
+                             {
+                                 newRow[column.ColumnName] = genelToplamResults[0].vdmBilgiRow[column];
+                             }
+
+                             genelToplamTable.Rows.Add(newRow);
+
+                             SetDataTable_To_Excel(genelToplamTable, vdmKod + "_GenelToplam");
+                         }
+                     }
+
+
+                     MessageBox.Show("Genel Toplam Dosyaları Oluşturuldu.");
+                }
+                   
+
+                // Bayi İletişim Excel Dosyasını Seçme
+                OpenFileDialog openFileDialog3 = new OpenFileDialog()
+                {
+                    Filter = "Excel Dosyası |*.xlsx| Excel Dosyası|*.xls",
+                    Title = "Bayi İletişim Excel Dosyasını Seçiniz..",
+                };
+
+                if (openFileDialog3.ShowDialog() == DialogResult.OK)
+                {
+                    string mailFilePath = openFileDialog3.FileName;
+                    DataTable dtMailInfo = new DataTable();
+
+                    using (var workbook = new XLWorkbook(mailFilePath))
+                    {
+                        var worksheet = workbook.Worksheet(1);
+                        bool firstRow = true;
+                        foreach (var row in worksheet.Rows())
+                        {
+                            if (firstRow)
+                            {
+                                foreach (var cell in row.Cells())
+                                    dtMailInfo.Columns.Add(cell.Value.ToString());
+                                firstRow = false;
+                            }
+                            else
+                            {
+                                dtMailInfo.Rows.Add();
+                                int i = 0;
+                                foreach (var cell in row.Cells())
+                                    dtMailInfo.Rows[dtMailInfo.Rows.Count - 1][i++] = cell.Value;
+                            }
+                        }
+                    }
+                    string emailBody = mailBody.Text;
+
+                    foreach (DataRow row in dtMailInfo.Rows)
+                    {
+                        string VDMKodu = row["VDM_Kodu"].ToString();
+                        string email = row["Email"].ToString();
+                        // string attachmentPath = AppDomain.CurrentDomain.BaseDirectory.ToString() + @"ExcelExport\" + VDMKodu + ".xlsx";
+                        string attachmentPath = Path.Combine(folderPath, VDMKodu + ".xlsx");
+                        string attachmentPath2 = Path.Combine(folderPath, VDMKodu + "_GenelToplam.xlsx");
+                        if (File.Exists(attachmentPath))
+                        {
+                            SendEmail(email, "Excel Dosyanız", emailBody, attachmentPath);
+                        }
+                    }
+
+                    MessageBox.Show("Mailler Gönderildi.");
+                }
+            }
+        
+
+
+
+
+
+
+        }
+        private void SendEmail(string toEmail, string subject, string body, string attachmentPath)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("gurerg03@gmail.com");
+            mail.To.Add(toEmail);
+            mail.Subject = subject;
+            mail.Body = body;
+            Attachment attachment = new Attachment(attachmentPath);
+            mail.Attachments.Add(attachment);
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new NetworkCredential("gurerg03@gmail.com", "rnur scsl vydm czbm");
+            SmtpServer.EnableSsl = true;
+
+            try
+            {
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mail gönderilemedi: " + ex.Message);
+            }
+        }
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void EXCELL_APPLİCATİON_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
